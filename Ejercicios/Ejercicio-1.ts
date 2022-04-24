4831c91b3f7fbf23fd65d9526cfbd03b6cea7d06
Pedro Ibarra <103613346+Piter31@users.noreply.github.com>
2022-04-24 16:58:29 -0300

    Initial commit

---
+++ Ejercicios/Ejercicio-1.ts
@@ -0,0 +1,9 @@
+console.log("avanzar");
+console.log("eliminar");
+console.log("avanzar");
+console.log("eliminar");
+console.log("avanzar");
+console.log("eliminar");
+console.log("avanzar");
+console.log("eliminar");
+console.log("girar 90° a la izquierda");
