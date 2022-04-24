922fc0ce7b0dc358f606f3d88d92dea8f1daecd6
Pedro Ibarra <103613346+Piter31@users.noreply.github.com>
2022-04-24 17:36:34 -0300

    agregando ejercicios

---
+++ Ejercicios/Ejercicio-3.ts
@@ -0,0 +1,9 @@
+//esquiva la vaca y elimina el monto de tierra
+console.log("girar 90° a la izquierda");
+console.log("avanzar");
+console.log("girar 90° a la derecha");
+console.log("avanzar");
+console.log("avanzar");
+console.log("girar 90° a la derecha");
+console.log("avanzar");
+console.log("eliminar");
