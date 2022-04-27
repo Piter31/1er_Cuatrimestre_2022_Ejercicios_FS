eb49d86a10f8961af8f936c68b3df0c9d6ca6ab4
Pedro Ibarra <103613346+Piter31@users.noreply.github.com>
2022-04-27 02:23:17 -0300

    nuevos cambios en el ultimo ejercicio

--- Ejercicio_25-04-22/datos.ts
+++ Ejercicio_25-04-22/datos.ts
@@ -1,7 +1,6 @@
 let btnEnv = document.getElementById("btnEnviar");
 let dato = document.getElementById("dato");
 let rotulo = document.getElementById("rotulo");
-
 rotulo.innerHTML = "Ingrese un dato: ";
 btnEnv.addEventListener("click", () => {
   //Tu codigo va aqui, por ejemplo:
