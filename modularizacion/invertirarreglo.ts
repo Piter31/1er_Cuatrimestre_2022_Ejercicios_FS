9b3557445f319ab84ae84077da805c157da86eef
Pedro M Ibarra <103613346+Piter31@users.noreply.github.com>
2022-05-30 20:11:27 -0300

    agregando codigo

--- modularizacion/invertirarreglo.ts
+++ modularizacion/invertirarreglo.ts
@@ -1,10 +1,11 @@
 // let dimensionArreglo: number = 6;
 let v1: number[] = [1, 2, 3, 4, 5];
+console.log("vector original: ", v1);
 
 function mostrarVectorInvertido(vector: number[]) {
-	for (let indice: number = vector.length - 1; indice >= 0; indice--) {
-		console.log(vector[indice]);
-	}
+  for (let indice: number = vector.length - 1; indice >= 0; indice--) {
+    console.log(vector[indice]);
+  }
 }
 
 mostrarVectorInvertido(v1);
