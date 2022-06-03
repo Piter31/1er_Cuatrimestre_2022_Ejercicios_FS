6f3c9877497c7493eaf743c92cebf45b2c6f39c6
Pedro M Ibarra <103613346+Piter31@users.noreply.github.com>
2022-06-03 01:54:42 -0300

    modificando ejercicios

--- modularizacion/numeros.ts
+++
@@ -1,4 +0,0 @@
-//let num: number [] = new array [6];
-
-let num: number[] = [20, 13, 42, 0, 6];
-console.log(num);
---
+++ modularizacion/numerosDeseados.ts
@@ -0,0 +1,11 @@
+//let num: number [] = new Array [6];
+
+let num: number;
+let numeros: number[] = new Array(5);
+let indice: number;
+
+for (indice = 0; indice < 5; indice++) {
+  num = Number(prompt("Ingresar 5 numero diferentes:" + indice));
+  numeros[indice] = num;
+  console.log("Numero: " + num);
+}
